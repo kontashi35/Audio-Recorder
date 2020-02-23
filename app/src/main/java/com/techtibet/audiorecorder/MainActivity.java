2eabfb123cc7a2c49645fecad918b2d9d89d59e9
kontashi35 <khakontas33@gmail.com>
2020-02-23 20:53:48 +0530

    Initial commit

---
+++ app/src/main/java/com/techtibet/audiorecorder/MainActivity.java
@@ -0,0 +1,16 @@
+package com.techtibet.audiorecorder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+/**
+ * Created by kharag on 22-02-2020.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
